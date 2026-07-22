d9a60edf654c92ab5ee6d58a4d561d2364d691cb
Aqsaabbasi199 <abbasiaqsa199@gmail.com>
2026-07-22 08:01:54 -0700

    chapter2 done

---
+++ chapter-4/app.js
@@ -0,0 +1,32 @@
+// Question no 1
+
+// var myName = "Aqsa" , age = 18 , city = "karachi";
+// document.write (myName + "  ", age + "  " , city)
+
+// Question no 2
+
+// legal variables names:
+// 1: var myName
+// 2: var myName7
+// 3: var my_Name
+// 4: var $myName
+// 5: var myNameaqsa
+
+// illegal variables names:
+// 1: var myname
+// 2: var 1myName
+// 3: var my Name
+// 4: var var
+// 5: var @myName
+
+// Question no 3
+// a
+document.write("<h1>Rules for naming js variables </h1>")
+// b
+document.write("variables names can only contain letters, numbers, and $, _ .<br>")
+// c
+document.write("variables must begin with a letter, $, or _ .<br>")
+// d
+document.write("variables names are case sensitive. <br>")
+// e
+document.write("variables names should not be js keywords.")
