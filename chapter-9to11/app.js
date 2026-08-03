f88b6c1b5093c5f18be5afb234692ca7de6067ca
Aqsaabbasi199 <abbasiaqsa199@gmail.com>
2026-08-03 15:03:35 -0700

    done

---
+++ chapter-9to11/app.js
@@ -0,0 +1,43 @@
+// Question No 1
+var city = prompt("Enter Your City Name")
+if (city === "karachi") {
+    alert("Welcome To City Of Lights")
+}
+
+
+// Question No 2
+var gender = prompt("Enter Your Gender")
+if (gender === "male") {
+    alert("Good Morning Sir")
+}
+if (gender === "female") {
+    alert("Good Morning Ma'am")
+}
+
+
+// Question No 3
+var color = prompt("Enter Traffic Signal Color")
+if (color === "red") {
+    alert("Must stop")
+}
+if (color === "yellow") {
+    alert("Ready To Move")
+}
+
+if (color === "green") {
+    alert("Move now")
+}
+
+
+// Question No 4
+var fuel = prompt("Enter remaining fuel in litres")
+if (fuel < 0.25) {
+    alert("Please refill the fuel in your car")
+}
+
+// Question No 5
+var  a = 4
+if (++a === 5) {
+    alert("given condition for variable a is true")
+}
+
