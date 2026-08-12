3e695b37c5a8be86e32c7c3b474daccb26f43aa8
Aqsaabbasi199 <abbasiaqsa199@gmail.com>
2026-08-12 13:50:31 -0700

    done

---
+++ chapter 14-16/app.js
@@ -0,0 +1,19 @@
+// Question No 1
+
+var studentNames = [];
+
+// Question No 1
+
+var studentNames = new Array();
+
+// Question No 1
+
+var names = ["Ali", "Ahmed", "Sara"];
+
+// Question No 1
+
+var numbers = [10, 20, 30, 40];
+
+// Question No 1
+
+var values = [true, false, true, false];
